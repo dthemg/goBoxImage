05cfc0a39b6a6163c0d70bb114ebef90faacafc4
dthemg <david.montgomery@live.se>
2020-11-28 21:12:12 +0100

    Box-art!

---
+++ README.md
@@ -0,0 +1,7 @@
+# Boxifying images
+
+Convert an image into a arty box-style image.
+
+![Image of a kingfisher](https://github.com/dthemg/goBoxImage/blob/master/resources/kingfisher.jpg)
+
+![Image of a arty kingfisher](https://github.com/dthemg/goBoxImage/blob/master/resources/out.png)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
+	"math/rand"
 	"os"
 
 	"github.com/fogleman/gg"
@@ -16,14 +17,11 @@ type pixel struct {
 	b uint32
 }
 
-const width int = 1000
-const height int = 1000
-
 func main() {
 	// Set format
 	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
 
-	imgFile, err := os.Open("./me.jpg")
+	imgFile, err := os.Open("resources/kingfisher.jpg")
 
 	if err != nil {
 		fmt.Println("img.jpg not found")
@@ -50,42 +48,66 @@ func main() {
 		os.Exit(1)
 	}
 
-	var rgbArr [649][649]pixel
+	// Read pixel values into array
+	var rgbArr [][]pixel
 	for w := 0; w < width; w++ {
+		var row []pixel
 		for h := 0; h < height; h++ {
 			r, g, b, _ := img.At(w, h).RGBA()
-			rgbArr[w][h] = pixel{r, g, b}
+			row = append(row, pixel{r, g, b})
 		}
+		rgbArr = append(rgbArr, row)
 	}
 
-	const gs int = 20
-	const gsSq = float64(gs * gs)
 	const maxPxval float64 = 65535
 
 	canvas := gg.NewContext(width, height)
 	canvas.SetHexColor("#0000ff")
-	canvas.SetLineWidth(2)
 
-	for wg := 0; wg < 649/gs; wg++ {
-		for hg := 0; hg < 649/gs; hg++ {
-			var rSum float64 = 0
-			var gSum float64 = 0
-			var bSum float64 = 0
-			for i := 0; i < gs; i++ {
-				for j := 0; j < gs; j++ {
-					pixel := rgbArr[wg*gs+i][hg*gs+j]
-					rSum += float64(pixel.r)
-					gSum += float64(pixel.g)
-					bSum += float64(pixel.b)
-				}
-			}
-			var rpx = rSum / gsSq / maxPxval
-			var gpx = gSum / gsSq / maxPxval
-			var bpx = bSum / gsSq / maxPxval
-			canvas.DrawCircle(float64(wg*gs), float64(hg*gs), float64(gs)/2.)
-			canvas.SetRGB(rpx, gpx, bpx)
-			canvas.Fill()
+	nSquares := 10000
+	for n := 0; n < nSquares; n++ {
+		wIdx := rand.Intn(width)
+		hIdx := rand.Intn(height)
+
+		wSz := 5 + rand.Intn(100)
+		hSz := 5 + rand.Intn(100)
+
+		wSzCap := wSz
+		if wIdx+wSz >= width {
+			wSzCap = width - wIdx
 		}
+		hSzCap := hSz
+		if hIdx+hSz >= height {
+			hSzCap = height - hIdx
+		}
+
+		var rSum float64 = 0
+		var gSum float64 = 0
+		var bSum float64 = 0
+
+		for i := wIdx; i < wIdx+wSzCap; i++ {
+			for j := hIdx; j < hIdx+hSzCap; j++ {
+				pixel := rgbArr[i][j]
+				rSum += float64(pixel.r)
+				gSum += float64(pixel.g)
+				bSum += float64(pixel.b)
+			}
+		}
+		wSzCapFloat := float64(wSzCap)
+		hSzCapFloat := float64(hSzCap)
+		var rpx = rSum / wSzCapFloat / hSzCapFloat / maxPxval
+		var gpx = gSum / wSzCapFloat / hSzCapFloat / maxPxval
+		var bpx = bSum / wSzCapFloat / hSzCapFloat / maxPxval
+
+		canvas.DrawRectangle(
+			float64(wIdx-wSz/2),
+			float64(hIdx-hSz/2),
+			float64(wSz),
+			float64(hSz),
+		)
+		canvas.SetRGBA(rpx, gpx, bpx, 0.5)
+		canvas.Fill()
 	}
+
 	canvas.SavePNG("out.png")
 }
